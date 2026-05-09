c422490d6b605688019905b6012da5917078728d
Sergey Yaskov <yaskovdev@gmail.com>
2026-05-09 17:48:53 +0300

    First working version.

---
+++ StationeersCombustionFix/CombustionPatch.cs
@@ -0,0 +1,28 @@
+﻿using static Assets.Scripts.Atmospherics.Chemistry;
+
+namespace StationeersCombustionFix;
+
+using Assets.Scripts.Atmospherics;
+using HarmonyLib;
+
+[HarmonyPatch(typeof(Combustion), nameof(Combustion.CombustMoles))]
+internal class CombustionPatch
+{
+    private static bool Prefix(Mole fuel, Mole oxidiser, double combustionRatio, out MoleEnergy combustionEnergy, out MoleQuantity combustedFuel, out float cleanBurnRatio, ref GasMixture __result)
+    {
+        var combustionResult = (fuel.Type, oxidiser.Type) switch
+        {
+            (GasType.Methane, GasType.Oxygen) => new CombustionResult(1, 2, new CombustionValue[] { new(GasType.CarbonDioxide, 1), new(GasType.Steam, 2) }),
+            _ => CombustionResult.Invalid
+        };
+        if (combustionResult.IsValid())
+        {
+            __result = combustionResult.RunCombustion(fuel, oxidiser, combustionRatio, out combustionEnergy, out combustedFuel, out cleanBurnRatio);
+            return false;
+        }
+        combustionEnergy = MoleEnergy.Zero;
+        combustedFuel = MoleQuantity.Zero;
+        cleanBurnRatio = 1f;
+        return true;
+    }
+}
--- StationeersCombustionFix/Plugin.cs
+++ StationeersCombustionFix/Plugin.cs
@@ -1,16 +1,19 @@
-﻿using BepInEx;
-using BepInEx.Logging;
+﻿namespace StationeersCombustionFix;
 
-namespace StationeersCombustionFix;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 public class Plugin : BaseUnityPlugin
 {
-    internal static new ManualLogSource Logger;
-        
+    internal static new ManualLogSource? Logger;
+
     private void Awake()
     {
         // Plugin startup logic
+        var harmony = new Harmony("com.yaskovdev.stationeerscombustionfix");
+        harmony.PatchAll(typeof(Plugin).Assembly);
         Logger = base.Logger;
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
     }
